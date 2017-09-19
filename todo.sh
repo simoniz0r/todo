01f2e0a78d936dbbaf87c2beb13033fc16bfdaf9
simoniz0r <bnu.fantasma@gmail.com>
2017-09-19 16:20:48 -0500

    Add #2 set todo directory with $TODO_DIR environment variable $HOME/.todo will still be used by default

--- todo.sh
+++ todo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A simple script that manages todo lists in ~/.todo/
+# A simple script that manages todo lists in "$TODO_DIR"/
 # Written by simonizor http://simonizor.gq/linuxapps
 # Dependencies: coreutils
 # License: GPL v2 Only
@@ -32,7 +32,7 @@ todo usage examples:
 printf "
 Usage: todo [OPTION] [ITEM]
 
-todo manages todo lists in an easy to use manner.  Lists are stored in ~/.todo/ListName
+todo manages todo lists in an easy to use manner.  Lists are stored in "$TODO_DIR"/ListName
 with each item in the list stored as a separate file in that directory.
 
 Options:
@@ -67,10 +67,10 @@ todoaddfunc () {
         helpfunc
         exit 1
     fi
-    if [ -d ~/.todo/"$LIST" ]; then
-        FILE_NAME="$(($(dir ~/.todo/"$LIST" | wc -w)+1))"
+    if [ -d "$TODO_DIR"/"$LIST" ]; then
+        FILE_NAME="$(($(dir "$TODO_DIR"/"$LIST" | wc -w)+1))"
     else
-        mkdir ~/.todo/"$LIST"
+        mkdir "$TODO_DIR"/"$LIST"
         FILE_NAME="1"
     fi
     if echo -e "$@" | cut -f3 -d" " | grep -q '='; then
@@ -78,28 +78,28 @@ todoaddfunc () {
         TODO_ITEM="$(echo -e "$@" | cut -f4- -d" ")"
         case $IMPORTANT_LEVEL in
             4)
-                echo -e "- \e[31m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+                echo -e "- \e[31m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                 echo -e "Item \"\e[31m$TODO_ITEM\e[39m\" added to $LIST list!"
                 ;;
             3)
-                echo -e "- \e[33m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+                echo -e "- \e[33m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                 echo -e "Item \"\e[33m$TODO_ITEM\e[39m\" added to $LIST list!"
                 ;;
             2)
-                echo -e "- \e[32m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+                echo -e "- \e[32m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                 echo -e "Item \"\e[32m$TODO_ITEM\e[39m\" added to $LIST list!"
                 ;;
             0)
-                echo -e "- \e[90m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+                echo -e "- \e[90m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                 echo -e "Item \"\e[90m$TODO_ITEM\e[39m\" added to $LIST list!"
                 ;;
             *)
-                echo -e "- \e[39m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+                echo -e "- \e[39m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                 echo -e "Item \"\e[39m$TODO_ITEM\e[39m\" added to $LIST list!"
                 ;;
         esac
     else
-        echo -e "- \e[39m$TODO_ITEM\e[39m" > ~/.todo/"$LIST"/"$FILE_NAME"
+        echo -e "- \e[39m$TODO_ITEM\e[39m" > "$TODO_DIR"/"$LIST"/"$FILE_NAME"
         echo -e "Item \"$TODO_ITEM\" added to $LIST list!"
     fi
 }
@@ -113,31 +113,31 @@ todoeditfunc () {
     if echo -e "$@" | cut -f4 -d" " | grep -q '='; then
         IMPORTANT_LEVEL="$(echo -e "$@" | cut -f2 -d"=" | cut -f1 -d" ")"
         TODO_ITEM="$(echo -e "$@" | cut -f3 -d" ")"
-        if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
+        if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
             case $IMPORTANT_LEVEL in
                 4)
-                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[31m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[31m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[31m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[31m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                     echo -e "Item \"$TODO_ITEM\" in $LIST changed to importance level 4!"
                     ;;
                 3)
-                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[33m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[33m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[33m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[33m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                     echo -e "Item \"$TODO_ITEM\" in $LIST changed to importance level 3!"
                     ;;
                 2)
-                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[32m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[32m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[32m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[32m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                     echo -e "Item \"$TODO_ITEM\" in $LIST changed to importance level 2!"
                     ;;
                 0)
-                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[90m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[90m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[90m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[90m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                     echo -e "Item \"$TODO_ITEM\" in $LIST changed to importance level 0!"
                     ;;
                 *)
-                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[39m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[39m%g' ~/.todo/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%- \x1b\[[0-9;]*m%- \x1b\[39m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    sed -i 's%✘ \x1b\[[0-9;]*m%✘ \x1b\[39m%g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                     echo -e "Item \"$TODO_ITEM\" in $LIST changed to importance level 1!"
                     ;;
             esac
@@ -147,8 +147,8 @@ todoeditfunc () {
         fi
     else
         TODO_ITEM="$(echo -e "$@" | cut -f3 -d" ")"
-        if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
-            $EDITOR ~/.todo/"$LIST"/"$TODO_ITEM"
+        if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
+            $EDITOR "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
         else
             echo -e "Item $TODO_ITEM not found in $LIST!"
             exit 1
@@ -166,10 +166,10 @@ tododonefunc () {
     case $TODO_ITEM in
         all)
             echo "Marking all items in $LIST as done..."
-            for TODO_ITEM in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
-                if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
-                    sed -i 's%- %✘ %g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    cat ~/.todo/"$LIST"/"$TODO_ITEM"
+            for TODO_ITEM in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
+                if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
+                    sed -i 's%- %✘ %g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    cat "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                 else
                     echo -e "Item $TODO_ITEM not found in $LIST!"
                     exit 1
@@ -177,10 +177,10 @@ tododonefunc () {
             done
             ;;
         *)
-            if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
-                sed -i 's%- %✘ %g' ~/.todo/"$LIST"/"$TODO_ITEM"
+            if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
+                sed -i 's%- %✘ %g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                 echo -e "Item $TODO_ITEM marked as done in $LIST!"
-                cat ~/.todo/"$LIST"/"$TODO_ITEM"
+                cat "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
             else
                 echo -e "Item $TODO_ITEM not found in $LIST!"
                 exit 1
@@ -199,10 +199,10 @@ todoundofunc () {
     case $TODO_ITEM in
         all)
             echo "Marking all items in $LIST as not done..."
-            for TODO_ITEM in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
-                if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
-                    sed -i 's%✘ %- %g' ~/.todo/"$LIST"/"$TODO_ITEM"
-                    cat ~/.todo/"$LIST"/"$TODO_ITEM"
+            for TODO_ITEM in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
+                if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
+                    sed -i 's%✘ %- %g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                    cat "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                 else
                     echo -e "Item $TODO_ITEM not found in $LIST!"
                     exit 1
@@ -210,10 +210,10 @@ todoundofunc () {
             done
             ;;
         *)
-            if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
-                sed -i 's%✘ %- %g' ~/.todo/"$LIST"/"$TODO_ITEM"
+            if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
+                sed -i 's%✘ %- %g' "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
                 echo -e "Item $TODO_ITEM marked as not done in $LIST!"
-                cat ~/.todo/"$LIST"/"$TODO_ITEM"
+                cat "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
             else
                 echo -e "Item $TODO_ITEM not found in $LIST!"
                 exit 1
@@ -241,7 +241,7 @@ todormfunc () {
             echo
             case $RMANSWER in
                 y*|Y*)
-                    rm -rf ~/.todo/"$LIST"
+                    rm -rf "$TODO_DIR"/"$LIST"
                     echo -e "All items in $LIST have been removed!"
                     ;;
                 *)
@@ -256,17 +256,17 @@ todormfunc () {
                 helpfunc
                 exit 1
             fi
-            if [ -f ~/.todo/"$LIST"/"$TODO_ITEM" ]; then
+            if [ -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM" ]; then
                 echo -e "Item $TODO_ITEM removed from $LIST!"
-                cat ~/.todo/"$LIST"/"$TODO_ITEM"
-                rm ~/.todo/"$LIST"/"$TODO_ITEM"
-                if [ "$(dir ~/.todo/"$LIST" | wc -w)" = "0" ]; then
-                    rm -r ~/.todo/"$LIST"
+                cat "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                rm "$TODO_DIR"/"$LIST"/"$TODO_ITEM"
+                if [ "$(dir "$TODO_DIR"/"$LIST" | wc -w)" = "0" ]; then
+                    rm -r "$TODO_DIR"/"$LIST"
                 else
-                    for file in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
+                    for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
                         if [ "$file" -gt "$TODO_ITEM" ]; then
                             FILE_NAME="$(($file-1))"
-                            mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$FILE_NAME"
+                            mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$FILE_NAME"
                         fi
                     done
                 fi
@@ -279,61 +279,61 @@ todormfunc () {
 }
 
 todomvwithinlistfunc () {
-    if [ ! -f ~/.todo/"$LIST"/"$TODO_ITEM_1" ] || [ ! -f ~/.todo/"$LIST"/"$TODO_ITEM_2" ]; then
+    if [ ! -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1" ] || [ ! -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM_2" ]; then
         echo "Items $TODO_ITEM_1 and/or $TODO_ITEM_2 not found in $LIST!"
         exit 1
     fi
-    mv ~/.todo/"$LIST"/"$TODO_ITEM_1" ~/.todo/"$LIST"/"$TODO_ITEM_1"-save
+    mv "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1" "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1"-save
     if [ "$TODO_ITEM_1" -gt "$TODO_ITEM_2" ]; then
-        for file in $(dir -C -w 1 ~/.todo/"$LIST" | grep -v '.*-save' | sort -n); do
+        for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | grep -v '.*-save' | sort -n); do
             if [ "$file" -le "$TODO_ITEM_1" ] && [ "$file" -ge "$TODO_ITEM_2" ]; then
-                mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$file"-temp
+                mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$file"-temp
             fi
         done
-        for file in $(dir -C -w 1 ~/.todo/"$LIST" | grep '.*-temp' | sort -n); do
+        for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | grep '.*-temp' | sort -n); do
             FILE_NAME="$(echo "$file" | cut -f1 -d"-")"
             if [ "$FILE_NAME" -le "$TODO_ITEM_1" ]; then
                 FILE_NAME="$(($FILE_NAME+1))"
-                mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$FILE_NAME"
+                mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$FILE_NAME"
             fi
         done
     else
-        for file in $(dir -C -w 1 ~/.todo/"$LIST" | grep -v '.*-save' | sort -n); do
+        for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | grep -v '.*-save' | sort -n); do
             if [ "$file" -le "$TODO_ITEM_2" ] && [ "$file" -ge "$TODO_ITEM_1" ]; then
-                mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$file"-temp
+                mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$file"-temp
             fi
         done
-        for file in $(dir -C -w 1 ~/.todo/"$LIST" | grep '.*-temp' | sort -n); do
+        for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | grep '.*-temp' | sort -n); do
             FILE_NAME="$(echo "$file" | cut -f1 -d"-")"
             if [ "$FILE_NAME" -le "$TODO_ITEM_2" ]; then
                 FILE_NAME="$(($FILE_NAME-1))"
-                mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$FILE_NAME"
+                mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$FILE_NAME"
             fi
         done
     fi
-    mv ~/.todo/"$LIST"/"$TODO_ITEM_1"-save ~/.todo/"$LIST"/"$TODO_ITEM_2"
+    mv "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1"-save "$TODO_DIR"/"$LIST"/"$TODO_ITEM_2"
     echo "Item $TODO_ITEM_1 moved to position $TODO_ITEM_2 in $LIST!"
 }
 
 todomvitemlistfunc () {
     LIST_2="$TODO_ITEM_2"
-    if [ ! -f ~/.todo/"$LIST"/"$TODO_ITEM_1" ]; then
+    if [ ! -f "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1" ]; then
         echo "Item $TODO_ITEM_1 not found in $LIST!"
         exit 1
     fi
-    if [ ! -d ~/.todo/"$LIST_2" ]; then
-        mkdir ~/.todo/"$LIST_2"
+    if [ ! -d "$TODO_DIR"/"$LIST_2" ]; then
+        mkdir "$TODO_DIR"/"$LIST_2"
     fi
-    FILE_NAME="$(($(dir ~/.todo/"$LIST_2" | wc -w)+1))"
-    mv ~/.todo/"$LIST"/"$TODO_ITEM_1" ~/.todo/"$LIST_2"/"$FILE_NAME"
+    FILE_NAME="$(($(dir "$TODO_DIR"/"$LIST_2" | wc -w)+1))"
+    mv "$TODO_DIR"/"$LIST"/"$TODO_ITEM_1" "$TODO_DIR"/"$LIST_2"/"$FILE_NAME"
     echo "Item $TODO_ITEM_1 has been moved from $LIST to $LIST_2!"
-    if [ "$(dir ~/.todo/"$LIST" | wc -w)" = "0" ]; then
-        rm -r ~/.todo/"$LIST"
+    if [ "$(dir "$TODO_DIR"/"$LIST" | wc -w)" = "0" ]; then
+        rm -r "$TODO_DIR"/"$LIST"
     else
-        for file in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
+        for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
             if [ "$file" -gt "$TODO_ITEM_1" ]; then
                 FILE_NAME="$(($file-1))"
-                mv ~/.todo/"$LIST"/"$file" ~/.todo/"$LIST"/"$FILE_NAME"
+                mv "$TODO_DIR"/"$LIST"/"$file" "$TODO_DIR"/"$LIST"/"$FILE_NAME"
             fi
         done
     fi
@@ -341,15 +341,15 @@ todomvitemlistfunc () {
 
 todomvlistlistfunc () {
     LIST_2="$TODO_ITEM_1"
-    if [ ! -d ~/.todo/"$LIST_2" ]; then
-        mkdir ~/.todo/"$LIST_2"
+    if [ ! -d "$TODO_DIR"/"$LIST_2" ]; then
+        mkdir "$TODO_DIR"/"$LIST_2"
     fi
-    for item in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
-        FILE_NAME="$(($(dir ~/.todo/"$LIST_2" | wc -w)+1))"
-        mv ~/.todo/"$LIST"/"$item" ~/.todo/"$LIST_2"/"$FILE_NAME"
+    for item in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
+        FILE_NAME="$(($(dir "$TODO_DIR"/"$LIST_2" | wc -w)+1))"
+        mv "$TODO_DIR"/"$LIST"/"$item" "$TODO_DIR"/"$LIST_2"/"$FILE_NAME"
     done
     echo "All items in $LIST moved to $LIST_2!"
-    rm -r ~/.todo/"$LIST"
+    rm -r "$TODO_DIR"/"$LIST"
 }
 
 todomvfunc () {
@@ -357,7 +357,7 @@ todomvfunc () {
     if [ -z "$LIST" ]; then
         helpfunc
         exit 1
-    elif [ ! -d ~/.todo/"$LIST" ]; then
+    elif [ ! -d "$TODO_DIR"/"$LIST" ]; then
         echo "$LIST does not exist!"
         exit 1
     fi
@@ -389,7 +389,7 @@ todomvfunc () {
 
 todolistfunc () {
     LIST="$(echo -e "$@" | cut -f2 -d" ")"
-    if [ "$(dir ~/.todo | wc -w)" = "0" ]; then
+    if [ "$(dir "$TODO_DIR" | wc -w)" = "0" ]; then
         echo -e "No lists made yet!"
         echo
         helpfunc
@@ -399,26 +399,26 @@ todolistfunc () {
         echo
         echo -e "$(tput bold)All todo lists$(tput sgr0):"
         echo
-        for dir in $(dir ~/.todo); do
+        for dir in $(dir "$TODO_DIR"); do
             echo -e "$(tput bold)$dir$(tput sgr0):"
-            for file in $(dir -C -w 1 ~/.todo/"$dir" | sort -n); do
+            for file in $(dir -C -w 1 "$TODO_DIR"/"$dir" | sort -n); do
                 if [ "$file" -lt "10" ]; then
-                    echo -e " $file $(cat ~/.todo/"$dir"/"$file")"
+                    echo -e " $file $(cat "$TODO_DIR"/"$dir"/"$file")"
                 else
-                    echo -e "$file $(cat ~/.todo/"$dir"/"$file")"
+                    echo -e "$file $(cat "$TODO_DIR"/"$dir"/"$file")"
                 fi
             done
             echo
         done
     else
-        if [ -d ~/.todo/"$LIST" ]; then
+        if [ -d "$TODO_DIR"/"$LIST" ]; then
                 echo
                 echo -e "$(tput bold)$LIST$(tput sgr0):"
-                for file in $(dir -C -w 1 ~/.todo/"$LIST" | sort -n); do
+                for file in $(dir -C -w 1 "$TODO_DIR"/"$LIST" | sort -n); do
                     if [ "$file" -lt "10" ]; then
-                        echo -e " $file $(cat ~/.todo/"$LIST"/"$file")"
+                        echo -e " $file $(cat "$TODO_DIR"/"$LIST"/"$file")"
                     else
-                        echo -e "$file $(cat ~/.todo/"$LIST"/"$file")"
+                        echo -e "$file $(cat "$TODO_DIR"/"$LIST"/"$file")"
                     fi
                 done
                 echo
@@ -429,18 +429,21 @@ todolistfunc () {
     fi
 }
 
-if [ ! -d ~/.todo ]; then
-    mkdir ~/.todo
+if [ -z "$TODO_DIR" ]; then
+    TODO_DIR="$HOME/.todo"
+fi
+if [ ! -d "$TODO_DIR" ]; then
+    mkdir "$TODO_DIR"
 fi
 
 if type todo >/dev/null 2>&1 && [ -f ~/.zshrc ] && ! grep -q 'todo.comp' ~/.zshrc; then
     REALPATH="$(readlink -f $0)"
     RUNNING_DIR="$(dirname "$REALPATH")"
     if [ -f "$RUNNING_DIR"/todo.comp ]; then
-        cp "$RUNNING_DIR"/todo.comp ~/.todo/.todo.comp
+        cp "$RUNNING_DIR"/todo.comp "$TODO_DIR"/.todo.comp
         echo "" >> ~/.zshrc
-        echo "if [ -f ~/.todo/.todo.comp ]; then" >> ~/.zshrc
-        echo "    source ~/.todo/.todo.comp" >> ~/.zshrc
+        echo "if [ -f "$TODO_DIR"/.todo.comp ]; then" >> ~/.zshrc
+        echo "    source "$TODO_DIR"/.todo.comp" >> ~/.zshrc
         echo "    compdef _todo todo" >> ~/.zshrc
         echo "fi" >> ~/.zshrc
     fi
